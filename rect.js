f998339c19b74a8ba800cb8026a3b8e85ab7b290
goliakshay357 <32712438+goliakshay357@users.noreply.github.com>
2019-07-09 11:02:38 +0300

    Add files via upload

--- rect.js
+++ rect.js
@@ -1,6 +1,6 @@
 var canvas
 var ctx
-
+var c,d;
 var strokes = []
 var currentStroke = null;
 
@@ -10,14 +10,23 @@ var brush = {
     x: 0,
     y: 0,
     color: '#000000',
-    size: 4,
+    size: 10,
     down: false
 }
 
+var initialx;
+var initialy;
+var finalx;
+var finaly;
+
+
+var temp = window.innerHeight;
+
 canvas = $('#draw');
 canvas.attr({
+
     width: window.innerWidth,
-    height: window.innerHeight,
+    height: temp,
 })
 ctx = canvas[0].getContext('2d');
 console.log(canvas);
@@ -35,14 +44,32 @@ function redraw() {
         ctx.strokeStyle = s.color;
         ctx.lineWidth = s.size;
         ctx.beginPath();
+        // data.line.startX;
 
         ctx.moveTo(s.points[0].x, s.points[0].y);
-        for (var j = 0; j < s.points.length; j++) {
-            var p = s.points[j];
-            ctx.lineTo(p.x, p.y);
+        // for (var j = 0; j < s.points.length; j++) {
+        //     var p = s.points[j];
+        //     ctx.lineTo(p.x, p.y);
+        //     // ctx.quadraticCurveTo(0,0,p.x, p.y)
+        
+        // }
+        // ctx.stroke();
+        for(var j=1; j<s.points.length-2;j++){
+            c = (s.points[j].x + s.points[j + 1].x) / 2
+            d = (s.points[j].y + s.points[j + 1].y) / 2;
+            ctx.quadraticCurveTo(s.points[j].x, s.points[j].y, c, d)
+            
         }
+        // ctx.quadraticCurveTo(s.points[i].x, s.points[i].y, s.points[i + 1].x, s.points[i + 1].y)
         ctx.stroke();
+
+
+
     }
+
+}
+
+function redraw_rect(){
 
 }
 
@@ -128,24 +155,83 @@ function init() {
     $('#clear-btn').click(function () {
         strokes = [];
         redraw();
+      
     });
     $('#color-picker').on('input', function() {
         brush.color = this.value;
     });
     $('#brush-size').on('input', function() {
         brush.size = this.value;
+        
     })
+
+    $('#newdown-btn').click(function () {
+        temp = temp + 500;
+        canvas.attr({
+            height : temp,
+        })
+    });
+
 
     $('#rect-btn').click( function(){
         draw = false
-        console.log(draw)
-    }
 
+        canvas.mousedown(function(e) {
+            
+            brush.x = e.pageX;
+            brush.y = e.pageY;
+            currentStroke.points.push({
+                x: brush.x,
+                y:brush.y
+            })
+            initialx = brush.x;
+            initialy = brush.y;
+            
+            ctx.beginPath();
+            ctx.lineWidth = "6";
+            ctx.strokeStyle = "red";
+            ctx.rect(initialx, initialy, finalx-initialx, finaly-initialy);  
+            ctx.stroke();
+        })
+
+        canvas.mousemove(function(e) {
+            brush.x = e.pageX;
+            brush.y = e.pageY;
+            
+            finalx = brush.x;
+            finaly = brush.y;
+
+            ctx.beginPath();
+            ctx.lineWidth = "6";
+            ctx.strokeStyle = "red";
+            ctx.rect(initialx, initialy, finalx-initialx, finaly-initialy);  
+            ctx.stroke();
+
+        })
+        canvas.mouseup(function(e) {
+            brush.x = e.pageX;
+            brush.y = e.pageY;
+            currentStroke.points.push({
+                x: brush.x,
+                y:brush.y
+            })
+            finalx = brush.x;
+            finaly = brush.y;
+
+            ctx.beginPath();
+            ctx.lineWidth = "6";
+            ctx.strokeStyle = "red";
+            ctx.rect(initialx, initialy, finalx-initialx, finaly-initialy);  
+            ctx.stroke();
+        })
         
+    }  
     )
 
 
 
 } //init closing tag
+
+
 
 $(init);
